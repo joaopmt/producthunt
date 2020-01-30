f9642b3f7cae4232a0d63f32efc36f24771381b9
Joao Pedro Martins <joaop.congiomartins@gmail.com>
2020-01-30 00:36:35 -0300

    setup sign up functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        pswd = request.POST['password']
+        conf_pswd = request.POST['conf_password']
+        if pswd == conf_pswd:
+            username = request.POST['username']
+            try:
+                user = User.objects.get(username=username)
+                return render(request, 'accounts/signup.html', {'error':'Username already taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(username, password=pswd)
+                auth.login(request, user)
+                return redirect('homepage')
+        else:
+            return render(request, 'accounts/signup.html', {'error':'Passwords didn\'t match'})
+    else:
+        return render(request, 'accounts/signup.html')
 
 
 def login(request):
