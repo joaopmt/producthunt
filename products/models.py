ec7a2f938d6f1b65c9c15098902090ff2cd3d8d9
Joao Pedro Martins <joaop.congiomartins@gmail.com>
2020-02-03 15:24:57 -0300

    add products model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    url = models.TextField()
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    body = models.TextField()
+    votes = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+        
+    def summary(self):
+        return self.body[:60] + '...'
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e, %Y')
