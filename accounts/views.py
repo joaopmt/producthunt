3f0b4047ca69123a58f2060bbd39fb7addd5102e
Joao Pedro Martins <joaop.congiomartins@gmail.com>
2020-02-03 12:54:01 -0300

    add login logout functionality

--- accounts/views.py
+++ accounts/views.py
@@ -4,27 +4,38 @@ from django.contrib import auth
 
 def signup(request):
     if request.method == 'POST':
+        username = request.POST['username']
         pswd = request.POST['password']
         conf_pswd = request.POST['conf_password']
-        if pswd == conf_pswd:
-            username = request.POST['username']
-            try:
-                user = User.objects.get(username=username)
-                return render(request, 'accounts/signup.html', {'error':'Username already taken'})
-            except User.DoesNotExist:
+        try:
+            user = User.objects.get(username=username)
+            return render(request, 'accounts/signup.html', {'error':'Username already taken'})
+        except User.DoesNotExist:
+            if pswd == conf_pswd:
                 user = User.objects.create_user(username, password=pswd)
                 auth.login(request, user)
                 return redirect('homepage')
-        else:
-            return render(request, 'accounts/signup.html', {'error':'Passwords didn\'t match'})
+            else:
+                return render(request, 'accounts/signup.html', {'error':'Passwords didn\'t match'})
     else:
         return render(request, 'accounts/signup.html')
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        pswd = request.POST['password']
+        user = auth.authenticate(username=username, password=pswd)
+        if user is None:
+            return render(request, 'accounts/login.html', {'error':'Authentication failed'})
+        else:
+            auth.login(request, user)
+            return redirect('homepage')
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    #TODO
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('homepage')
